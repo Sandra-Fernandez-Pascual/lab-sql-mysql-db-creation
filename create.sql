4d6d90538ec3d089b6f79a730475b9d64b757039
Sandra Fdez Pascual <sandrafdeztauro@gmail.com>
2026-07-06 21:09:36 +0200

    completed mysql lab

--- create.sql
+++ create.sql
@@ -3,18 +3,31 @@ USE lab_mysql;
 
 DROP TABLE IF EXISTS cars;
 
-CREATE TABLE cars (VIN INT, manufacturer VARCHAR(50), 
+CREATE TABLE cars (VIN INT AUTO_INCREMENT PRIMARY KEY, manufacturer VARCHAR(50), 
 model VARCHAR(20), year INT, color VARCHAR(10));
 
-CREATE TABLE customers (customer_id INT, customer_name VARCHAR(100), 
-phone_number INT, email VARCHAR(100), address VARCHAR(100), city VARCHAR(50),
+DROP TABLE IF EXISTS customers;
+
+CREATE TABLE customers (customer_id INT AUTO_INCREMENT PRIMARY KEY, customer_name VARCHAR(100), 
+phone_number INT, email VARCHAR(100), address VARCHAR(300), city VARCHAR(50),
  ZIP INT);
  
-CREATE TABLE sales_persons (staff_id INT, sales_person_name VARCHAR(100), 
- store VARCHAR(50));
+ DROP TABLE IF EXISTS sales_persons;
  
-CREATE TABLE invoices (invoice_number INT, date INT, car INT, customer INT,
-sales_person INT);
+CREATE TABLE sales_persons (staff_id INT AUTO_INCREMENT PRIMARY KEY, sales_person_name VARCHAR(100), 
+ store VARCHAR(300));
+ 
+ DROP TABLE IF EXISTS invoices;
+ 
+CREATE TABLE invoices (invoice_number INT AUTO_INCREMENT PRIMARY KEY, date INT, car INT, customer INT,
+sales_person INT)
+
+
+-- Define Foreign Keys here
+    FOREIGN KEY (car) REFERENCES cars(VIN),
+    FOREIGN KEY (customer) REFERENCES customers(customer_id),
+    FOREIGN KEY (sales_person) REFERENCES sales_persons(staff_id)
+);
 
  
  
--- seeding.sql
+++ seeding.sql
@@ -1,22 +1,22 @@
 USE lab_mysql;
 INSERT INTO cars (VIN, manufacturer, model, year, color)
-VALUES (52364, 'Volkswagen', 'Hatchback', 2010 , 'black'),
-       (89652, 'Tesla', 'Coupe', 2005, 'red'),
-       (35620, 'Renault', 'Van', 1999, 'grey');
+VALUES ('Volkswagen', 'Hatchback', 2010 , 'black'),
+       ('Tesla', 'Coupe', 2005, 'red'),
+       ('Renault', 'Van', 1999, 'grey');
        
 INSERT INTO customers (customer_id, customer_name, phone_number, email, address, city, ZIP)
-VALUES (0001, 'Jessica Miles', 015736980113, 'jessica.miles@email.com' , 'Kastanienallee 25', 'Berlin', 10435),
-       (0002, 'Jakob Schindler', 015760244926, 'jakob.schindler@email.com', 'Kollwitzstr. 35', 'Berlin', 10405),
-       (0003, 'Hans Van der Dijs', 015720164882, 'hans.v.Dijs@email.com', 'Turmstr. 20', 'Berlin', 10559);
+VALUES ('Jessica Miles', 15736980113, 'jessica.miles@email.com' , 'Kastanienallee 25', 'Berlin', 10435),
+       ('Jakob Schindler', 15760244926, 'jakob.schindler@email.com', 'Kollwitzstr. 35', 'Berlin', 10405),
+       ('Hans Van der Dijs', 15720164882, 'hans.v.Dijs@email.com', 'Turmstr. 20', 'Berlin', 10559);
        
    INSERT INTO sales_persons (staff_id, sales_person_name, store)
-VALUES (99105, 'Lisa Kiefer', 'Volkswagen Automobile Berlin Franklinstr. 5, 10587 Berlin'),
-       (98105, 'Elena Gutterberg', 'Tesla Center Berlin Reinickendorf Roedernallee 1-7, 13407 Berlin'),
-       (978105, 'Tobias Eriksson', 'Renault Berlin Tempelhof Germaniastr 145-149, 12099 Berlin');
+VALUES ('Lisa Kiefer', 'Volkswagen Automobile Berlin Franklinstr. 5, 10587 Berlin'),
+       ('Elena Gutterberg', 'Tesla Center Berlin Reinickendorf Roedernallee 1-7, 13407 Berlin'),
+       ('Tobias Eriksson', 'Renault Berlin Tempelhof Germaniastr 145-149, 12099 Berlin');
        
        INSERT INTO invoices (invoice_number, date, car, customer, sales_person)
-VALUES (66001235, 1/03/2022, 'Volkswagen Golf', 'Jessica Miles','Lisa Kiefer'),
-       (50001235, 25/09/2025, 'Tesla Roadster','Jakob Schindler', 'Elena Gutterberg'),
-       (41001235, 16/07/2024, 'Renault Espace','Hans Van der Dijs','Tobias Eriksson');
+VALUES (1-03-2022),
+       (25-09-2025),
+       (16-07-2024);
        
        
